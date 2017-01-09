b43e8c8882c59a18f10abca4e9a8fb80370b12e5
btssio <btssio@btssio-VirtualBox>
2017-01-09 21:52:56 +0100

    3ème commit

--- include/_sommaire.inc.php
+++ include/_sommaire.inc.php
@@ -12,34 +12,34 @@ include ($repInclude . "connexionBDD.php");
     <div id="infosUtil">
         <?php
         if (estVisiteurConnecte()) {
-            $idUser = obtenirIdUserConnecte();
-            $lgUser = obtenirDetailVisiteur($idConnexion, $idUser);
-            $nom = $lgUser['nom'];
-            $prenom = $lgUser['prenom'];
-            $login = $lgUser['login'];
-            ?>
-            <h2>
-                <?php
-                echo $nom . " " . $prenom;
-                ?>
-            </h2>
+        $idUser = obtenirIdUserConnecte();
+        $lgUser = obtenirDetailVisiteur($idConnexion, $idUser);
+        $nom = $lgUser['nom'];
+        $prenom = $lgUser['prenom'];
+        $login = $lgUser['login'];
+        ?>
+        <h2>
             <?php
-            //different affichage selon la personne connecté (administrateur ou simple visiteur)
-            if ($login != 'admin') {
-                ?>
-                <h3>Employé</h3>  
-                <?php
-            } else {
-                echo '<h3>François Baraud</h3>';
-            }
+            echo $nom . " " . $prenom;
             ?>
-            <?php
+        </h2>
+        <?php
+        //different affichage selon la personne connecté (administrateur ou simple visiteur)
+        if ($login != 'admin') {
+        ?>
+        <h3>Employé</h3>  
+        <?php
+        } else {
+        echo '<h3>François Baraud</h3>';
+        }
+        ?>
+        <?php
         }
         ?>  
     </div>  
     <?php
     if (estVisiteurConnecte()) {
-        ?>
+    ?>
     <div class="corpsForm">
         <ul id="menuList">
             <li class="smenu">
@@ -63,29 +63,9 @@ include ($repInclude . "connexionBDD.php");
             <li class="smenu">
                 <a href="cModifierCharges.php" title="Charges">Charges</a>
             </li>  
- <?php
-            //accès aux fonctionnalités d'administration du site seulement possible pour l'administrateur du site
-            if ($login == 'admin') {
-                ?>
-                <li class="smenu">
-                    <a href="cConsulterBaseDeDonnee.php" title="Consultation de toutes les tables de la base de donnée">Affichage de la BDD</a>
-                </li>
-                <li class="smenu">
-                    <a href="cCrypterMotsDePasseBDD.php" title="Crypter les mots de passe de la base de donnée">Crypter mots de passe BDD</a>
-                </li>
-                <li class="smenu">
-                    <a href="cResetMotsDePasseBDD.php" title="Remettre les mots de passe de base dans la base de donnée">Reset mots de passe BDD</a>
-                </li>
-                </div>
-                <?php
-            }
-            ?>
-        </ul>
-        <?php
-        // affichage des éventuelles erreurs déjà détectées
-        if (nbErreurs($tabErreurs) > 0) {
-            echo toStringErreurs($tabErreurs);
-        }
+    </div>
+    <?php
     }
     ?>
+</ul>
 </div>
