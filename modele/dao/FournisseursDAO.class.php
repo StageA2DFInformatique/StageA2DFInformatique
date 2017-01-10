0abd612a2d250577c72c6a3863c405c9502e39b9
btssio <btssio@btssio-VirtualBox>
2017-01-10 16:47:17 +0100

    11ème commit

--- modele/dao/FournisseursDAO.class.php
+++ modele/dao/FournisseursDAO.class.php
@@ -2,15 +2,15 @@
 
 namespace modele\dao;
 
-use modele\metier\Etablissement;
+use modele\metier\Fournisseurs;
 use PDO;
 
 /**
- * Description of EtablissementDAO
- * Classe métier : Etablissement
+ * Description of FournisseursDAO
+ * Classe métier : Fournisseurs
  * @author btssio
  */
-class EtablissementDAO implements IDAO {
+class FournisseursDAO implements IDAO {
 
     protected static function enregVersMetier($enreg) {
         $id = $enreg['ID'];
@@ -25,14 +25,14 @@ class EtablissementDAO implements IDAO {
         $nomResp = $enreg[strtoupper('nomResponsable')];
         $prenomResp = $enreg[strtoupper('prenomResponsable')];
 
-        $unEtab = new Etablissement($id, $nom, $adresse, $cdp, $ville, $tel, $email, $type, $civResp, $nomResp, $prenomResp);
+        $unFourni = new Fournisseur($id, $nom, $adresse, $cdp, $ville, $tel, $email, $type, $civResp, $nomResp, $prenomResp);
 
-        return $unEtab;
+        return $unFourni;
     }
 
     /**
-     * Valorise les paramètre d'une requête préparée avec l'état d'un objet Etablissement
-     * @param type $objetMetier un Etablissement
+     * Valorise les paramètre d'une requête préparée avec l'état d'un objet Fournisseurs
+     * @param type $objetMetier un Fournisseurs
      * @param type $stmt requête préparée
      */
     protected static function metierVersEnreg($objetMetier, $stmt) {
@@ -52,11 +52,11 @@ class EtablissementDAO implements IDAO {
 
     /**
      * Insérer un nouvel enregistrement dans la table à partir de l'état d'un objet métier
-     * @param Etablissement $objet objet métier à insérer
+     * @param Fournisseur $objet objet métier à insérer
      * @return boolean =FALSE si l'opérationn échoue
      */
     public static function insert($objet) {
-        $requete = "INSERT INTO Etablissement VALUES (:id, :nom, :rue, :cdp, :ville, :tel, :email, :type, :civ, :nomResp, :prenomResp)";
+        $requete = "INSERT INTO Fournisseurs VALUES (:id, :nom, :rue, :cdp, :ville, :tel, :email, :type, :civ, :nomResp, :prenomResp)";
         $stmt = Bdd::getPdo()->prepare($requete);
         self::metierVersEnreg($objet, $stmt);
         $ok = $stmt->execute();
@@ -66,12 +66,12 @@ class EtablissementDAO implements IDAO {
     /**
      * Mettre à jour enregistrement dans la table à partir de l'état d'un objet métier
      * @param string identifiant de l'enregistrement à mettre à jour
-     * @param Etablissement $objet objet métier à mettre à jour
+     * @param Fournisseurs $objet objet métier à mettre à jour
      * @return boolean =FALSE si l'opérationn échoue
      */
     public static function update($id, $objet) {
         $ok = false;
-        $requete = "UPDATE  Etablissement SET NOM=:nom, ADRESSERUE=:rue,
+        $requete = "UPDATE  Fournisseurs SET NOM=:nom, ADRESSERUE=:rue,
            CODEPOSTAL=:cdp, VILLE=:ville, TEL=:tel,
            ADRESSEELECTRONIQUE=:email, TYPE=:type,
            CIVILITERESPONSABLE=:civ, NOMRESPONSABLE=:nomResp, PRENOMRESPONSABLE=:prenomResp 
@@ -85,7 +85,7 @@ class EtablissementDAO implements IDAO {
 
     public static function delete($id) {
         $ok = false;
-        $requete = "DELETE FROM Etablissement WHERE ID = :id";
+        $requete = "DELETE FROM Fournisseurs WHERE ID = :id";
         $stmt = Bdd::getPdo()->prepare($requete);
         $stmt->bindParam(':id', $id);
         $ok = $stmt->execute();
@@ -95,7 +95,7 @@ class EtablissementDAO implements IDAO {
 
     public static function getAll() {
         $lesObjets = array();
-        $requete = "SELECT * FROM Etablissement";
+        $requete = "SELECT * FROM Fournisseurs";
         $stmt = Bdd::getPdo()->prepare($requete);
         $ok = $stmt->execute();
         if ($ok) {
@@ -108,7 +108,7 @@ class EtablissementDAO implements IDAO {
 
     public static function getOneById($id) {
         $objetConstruit = null;
-        $requete = "SELECT * FROM Etablissement WHERE ID = :id";
+        $requete = "SELECT * FROM Fournisseurs WHERE ID = :id";
         $stmt = Bdd::getPdo()->prepare($requete);
         $stmt->bindParam(':id', $id);
         $ok = $stmt->execute();
@@ -120,16 +120,16 @@ class EtablissementDAO implements IDAO {
     }
 
     /**
-     * Retourne la liste des établissements qui ont enregistré des offres
-     * @return array tableau d'établissements
+     * Retourne la liste des fournisseurs qui ont enregistré des offres
+     * @return array tableau des fournisseurs
      */
     public static function getAllOfferingRooms() {
         $lesObjets = array();
-        $requete = "SELECT * FROM Etablissement 
+        $requete = "SELECT * FROM Fournisseurs 
                 WHERE ID IN 
                    (SELECT DISTINCT ID
                     FROM Offre o
-                    INNER JOIN Etablissement e ON e.ID = o.IDETAB
+                    INNER JOIN Fournisseurs e ON e.ID = o.IDETAB
                     ORDER BY ID)";
         $stmt = Bdd::getPdo()->prepare($requete);
         $ok = $stmt->execute();
@@ -140,14 +140,14 @@ class EtablissementDAO implements IDAO {
         }
         return $lesObjets;
     }
-    
+
     /**
-     * Permet de vérifier s'il existe ou non un établissement ayant déjà le même identifiant dans la BD
-     * @param string $id identifiant de l'établissement à tester
+     * Permet de vérifier s'il existe ou non un fournisseur ayant déjà le même identifiant dans la BD
+     * @param string $id identifiant du fournisseur à tester
      * @return boolean =true si l'id existe déjà, =false sinon
      */
     public static function isAnExistingId($id) {
-        $requete = "SELECT COUNT(*) FROM Etablissement WHERE ID=:id";
+        $requete = "SELECT COUNT(*) FROM Fournisseurs WHERE ID=:id";
         $stmt = Bdd::getPdo()->prepare($requete);
         $stmt->bindParam(':id', $id);
         $stmt->execute();
@@ -155,25 +155,25 @@ class EtablissementDAO implements IDAO {
     }
 
     /**
-     * Permet de vérifier s'il existe ou non un établissement portant déjà le même nom dans la BD
+     * Permet de vérifier s'il existe ou non un fournisseur portant déjà le même nom dans la BD
      * En mode modification, l'enregistrement en cours de modification est bien entendu exclu du test
      * @param boolean $estModeCreation =true si le test est fait en mode création, =false en mode modification
-     * @param string $id identifiant de l'établissement à tester
-     * @param string $nom nom de l'établissement à tester
+     * @param string $id identifiant du fournisseur à tester
+     * @param string $nom nom du fournisseur à tester
      * @return boolean =true si le nom existe déjà, =false sinon
      */
     public static function isAnExistingName($estModeCreation, $id, $nom) {
         $nom = str_replace("'", "''", $nom);
         // S'il s'agit d'une création, on vérifie juste la non existence du nom sinon
-        // on vérifie la non existence d'un autre établissement (id!='$id') portant 
+        // on vérifie la non existence d'un autre fournisseur (id!='$id') portant 
         // le même nom
         if ($estModeCreation) {
-            $requete = "SELECT COUNT(*) FROM Etablissement WHERE NOM=:nom";
+            $requete = "SELECT COUNT(*) FROM Fournisseurs WHERE NOM=:nom";
             $stmt = Bdd::getPdo()->prepare($requete);
             $stmt->bindParam(':nom', $nom);
             $stmt->execute();
         } else {
-            $requete = "SELECT COUNT(*) FROM Etablissement WHERE NOM=:nom AND ID<>:id";
+            $requete = "SELECT COUNT(*) FROM Fournisseurs WHERE NOM=:nom AND ID<>:id";
             $stmt = Bdd::getPdo()->prepare($requete);
             $stmt->bindParam(':id', $id);
             $stmt->bindParam(':nom', $nom);
