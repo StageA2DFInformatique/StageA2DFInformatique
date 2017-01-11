8b6d6e82f7616293ade3b51a1840d2109de808c4
btssio <btssio@btssio-VirtualBox>
2017-01-11 11:42:44 +0100

    13ème commit

--- include/_fin.inc.php
+++ include/_fin.inc.php
@@ -1,8 +1,23 @@
 <?php
-/** 
- * Libère les ressources nécessaires au fonctionnement de l'application
+/**
+ * Libï¿½re les ressources nï¿½cessaires au fonctionnement de l'application
  * @package default
  * @todo  RAS
  */
-    deconnecterServeurBD($idConnexion);
+deconnecterServeurBD($idConnexion);
 ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+    <head>
+        <title>Gestion A2DFInformatique</title>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <link href="./styles/styles.css" rel="stylesheet" type="text/css" />
+        <link href="./bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <div class="copy-right">
+            Copyright Â© Charly FLEURANCE 2017
+        </div>
+    </body>
