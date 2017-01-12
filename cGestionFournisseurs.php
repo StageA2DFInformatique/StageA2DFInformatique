a4e9264c0f8966fba99527b69f82533764b56640
btssio <btssio@btssio-VirtualBox>
2017-01-12 10:22:02 +0100

    17ème commit

--- cGestionFournisseurs.php
+++ cGestionFournisseurs.php
@@ -1,5 +1,4 @@
 <?php
-
 $repInclude = './include/';
 require($repInclude . "_init.inc.php");
 
@@ -10,14 +9,38 @@ if (!estVisiteurConnecte()) {
 require($repInclude . "_entete.inc.html");
 require($repInclude . "_sommaire.inc.php");
 ?>
-<?php
+<div id="contenu">
+    <h2>Gestion des Fournisseurs</h2>
+    <br>
+    <table width='55%' cellspacing='0' cellpadding='0' class='tabNonQuadrille'>
 
-//Division principale
-echo '<div id="contenu">';
-echo "<h2>Fournisseurs</h2>";
-echo '</div>';
-?>
-<?php
+        <tr class='enTeteTabNonQuad'>
+            <td colspan='4'><strong>Fournisseurs</strong></td>
+        </tr>
+        <tr class='ligneTabNonQuad'>
+            <td width='52%'>FournisseurTest</td>
 
+            <td width='20%' align='center'> 
+                <a href='cGestionFournisseurs.php?action=detailFourni&id=11111111'>
+                    <button type=button class="bouton-details">Voir détail</button></a></td>
+
+            <td width='16%' align='center'> 
+                <a href='cGestionFournisseurs.php?action=demanderModifierFourni&id=11111111'>
+                    <button type=button class="bouton-modifier">Modifier</button></a></td>
+            <td width='16%' align='center'> 
+                <a href='cGestionFournisseurs.php?action=demanderSupprimerFourni&id=11111111'>
+                    <button type=button class="bouton-supprimer">Supprimer</button></a></td>
+        </tr>
+    </table>
+    <br>
+    <div class="bouton">
+        <p>
+            <a href='cGestionFournisseurs.php?action=demanderCreerEtab'>
+                <button type=button>Ajout d'un fournisseur</button></a > 
+        </p> 
+    </div>
+    <br>
+</div>
+<?php
 require($repInclude . "_fin.inc.php");
 ?>
